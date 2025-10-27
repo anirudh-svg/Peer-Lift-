018b40d810fb7aa078abff9b89263c0fdad56c62
Anirudh <138357309+anirudh-svg@users.noreply.github.com>
2025-10-27 09:59:31 +0530

    Clean up  comments in dataModel.d.ts
    Removed autogenerated comments from dataModel.d.ts

--- convex/_generated/dataModel.d.ts
+++ convex/_generated/dataModel.d.ts
@@ -1,12 +1,4 @@
-/* eslint-disable */
-/**
- * Generated data model types.
- *
- * THIS CODE IS AUTOMATICALLY GENERATED.
- *
- * To regenerate, run `npx convex dev`.
- * @module
- */
+
 
 import type {
   DataModelFromSchemaDefinition,
