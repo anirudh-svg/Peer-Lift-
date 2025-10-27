3d73014e99db5d124f3d8fd3e2239c4b1b443b6f
Anirudh <138357309+anirudh-svg@users.noreply.github.com>
2025-10-27 10:01:36 +0530

    Remove  comments from server.js
    Removed autogenerated comments from server.js

--- convex/_generated/server.js
+++ convex/_generated/server.js
@@ -1,12 +1,4 @@
-/* eslint-disable */
-/**
- * Generated utilities for implementing server-side Convex query and mutation functions.
- *
- * THIS CODE IS AUTOMATICALLY GENERATED.
- *
- * To regenerate, run `npx convex dev`.
- * @module
- */
+
 
 import {
   actionGeneric,
